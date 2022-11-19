a4e2b8e115cfa57c259b87281c9b37bea519ae9d
Rahul <tehnorahul91@gmail.com>
2022-11-19 15:16:23 +0530

    React base file and css is added

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <h2 style={{background:'yellow',width:"30%" , padding:"3%",borderRadius:"20px"}}>New Admission</h2>
+    <Form/>
+   
+    </div>
+  );
+}
+
+
+export default App;
